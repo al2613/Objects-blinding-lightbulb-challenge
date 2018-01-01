baca978e0f62ecd85725455b003fe813d99427d5
Annette Lin <al261310@gmail.com>
2018-01-01 17:47:21 +0800

    Create challenge_code.js

---
+++ challenge_code.js
@@ -0,0 +1,23 @@
+var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+
+var lighthouseRock = {
+  gateClosed: true,
+  weaponBulbs: superBlinders,
+  capacity: 30,
+  secretPassageTo: "Underwater Outpost",
+  numRangers: 3,
+  ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
+  ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
+  ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1}
+};
+
+function dontPanic(location) {
+  var list = "Avast, me hearties!\n" +
+             "There be Pirates nearby! Stations!\n";
+
+  // create code here
+
+  alert(list); //make list variable pop up on screen
+}
+
+dontPanic(lighthouseRock); 
